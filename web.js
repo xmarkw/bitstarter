adf8a892e12456fcaddc90de5aee225670d6f0f2
Mark W <mark@workel.nl>
2013-07-10 11:34:14 +0000

    added fs.ReadfileSync

--- web.js
+++ web.js
@@ -4,13 +4,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-/*
+
 var fs = require("fs");
-var data = fs.readFileSync("foo.txt", "utf8");
-console.log(data);//  fs.readFileSync('index.html')
-  
-*/
-response.send('Hello World 2!');
+var data = fs.readFileSync("index.html", "utf8");
+response.send(data);
 
 });
 
