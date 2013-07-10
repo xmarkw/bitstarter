f550c71c176a1ff7105a8dc705bc783c20814457
Mark W <mark@workel.nl>
2013-07-10 11:26:17 +0000

    initial changes, index and web js

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
 
+//edit
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+/*
+var fs = require("fs");
+var data = fs.readFileSync("foo.txt", "utf8");
+console.log(data);//  fs.readFileSync('index.html')
+  
+*/
+response.send('Hello World 2!');
+
 });
 
 var port = process.env.PORT || 5000;
